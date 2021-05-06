da9c074a9718a657673c2c7750a46fec28b2b08b
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-05-06 11:03:48 -0400

    Initial

---
+++ CSV2PLSDAPlugin.py
@@ -0,0 +1,75 @@
+
+import PyPluMA
+
+
+import sys
+
+class CSV2PLSDAPlugin:
+   def input(self, filename):
+      # Parameter file
+      self.parameters = dict()
+      paramfile = open(filename, 'r')
+      for line in paramfile:
+         contents = line.split('\t')
+         self.parameters[contents[0]] = contents[1].strip()
+      normabund = open(PyPluMA.prefix()+"/"+self.parameters["normabund"], "r")
+      metadata = open(PyPluMA.prefix()+"/"+self.parameters["metadata"], "r")
+
+      metadata.readline()
+      self.categories = dict()
+      # Assuming two-column
+      self.diffcat = set()
+      for line in metadata:
+         line = line.strip()
+         contents = line.split(',')
+         self.categories[contents[0]] = contents[1]
+         self.diffcat.add(contents[1])
+
+
+      microbes = normabund.readline().strip()
+      self.contents = microbes.split(',')
+      self.contents = self.contents[1:]
+      self.lines = []
+      for line in normabund:
+          self.lines.append(line.strip())
+
+   def run(self):
+       pass
+
+   def output(self, filename):
+      obs_names = open(filename+"/"+self.parameters["categories"], 'w') # Categories, tabbed
+      var_ID = open(filename+"/"+self.parameters["observables"], 'w') # Microbes, one column
+      testsets = open(filename+"/"+self.parameters["targets"], 'w') # Different categories
+      normabundmod = open(filename+"/"+self.parameters["samples"], 'w')
+
+      for microbe in self.contents:
+          var_ID.write(microbe+"\n")
+
+      for entry in self.diffcat:
+         testsets.write(entry+"\n")
+
+      for j in range(0, len(self.lines)):
+          contents = self.lines[j].split(',')
+          sample = contents[0]
+          contents = contents[1:]
+          obs_names.write(self.categories[sample])
+          if (j != len(self.lines)-1):
+             obs_names.write('\t')
+
+      # Transpose
+      my_mat = []
+      for i in range(0, len(self.lines)):
+          my_mat.append(self.lines[i].strip().split(','))
+      
+      for j in range(1, len(my_mat[0])):
+         for i in range(0, len(my_mat)):
+             normabundmod.write(my_mat[i][j])
+             if (i != len(my_mat)-1):
+                 normabundmod.write(',')
+             else:
+                 normabundmod.write('\n')
+#abundmat = []
+
+
+#meta = []
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CSV2PLSDA
-Convert CSV file of abundances and groups into format to conduct PLSDA (Stahle and Wold, 1987)
+# Language: Python
+# Input: TXT (keyword-value pairs)
+# Output: PREFIX
+# Tested with: PluMA 1.1, Python 3.6
+# Dependency:
+
+PluMA plugin that converts abundance data into data that can
+be differentially analyzed by PLS-DA.
+
+The plugin accepts as input a tab-delimited parameter file of
+keyword value pairs:
+normabund: Normalized abundances (input)
+metadata: Samples and groups (input)
+samples: PLSDA-compatible samples file (output)
+categories: Category that each sample falls into (output)
+observables: Values to use for differential analysis
+targets: Unique groups
+
+All output files are prefixed by the user-specified output PREFIX.
